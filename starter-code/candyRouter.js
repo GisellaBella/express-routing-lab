84e58a43e2ed104d36c1fa6f9422e4acf7c416ff
Gisellabella <gisella@gmail.com>
2016-11-28 21:03:43 -0700

    latest

--- starter-code/candyRouter.js
+++ starter-code/candyRouter.js
@@ -2,16 +2,72 @@ var express = require('express');
 var bodyParser = require('body-parser');
 router = express.Router();
 
-var candies;
+var candy = [{"id":1,"name":"Chewing Gum","color":"Red"},{"id":2,"name":"Pez","color":"Green"},{"id":3,"name":"Marshmallow","color":"Pink"},{"id":4,"name":"Candy Stick","color":"Blue"}];
+console.log (candy);
 
-//What would need to go into candies
-//in order to pass our first test?
 
+//index
 router.get('/', function(req,res) {
-	// What would go here? 
-	// Hint: we want all candies in JSON format
+res.json(candy);
+res.send(candy); 
 });
 
 // Fill out the rest of the routes here
 
+
+//create
+router.post('/', function(req, res) {
+req.body.id = candy.length + 1;
+req.params.name=name;
+res.json(req.body);
+candy.push(req.body);
+res.send(candy);
+}); 
+
+
+
+
+//delete
+router.delete ('/:id', function(req, res) {
+	var current = req.params.id;
+	for (var i = 0 ; i<candy.length; i++) {
+		if (current ==candy[i].id ) {
+			candy.splice(i,1);
+		}
+	}
+	res.json(candy);
+	res.send(candy);
+	});
+
+
+
+
+
+//show
+router.get('/id:name', function(req, res) {
+var candyToShow = req.params.name;
+	for (var i = 0 ; i<candy.length; i++){
+		if (candyToShow.name == candy[i].id ) {
+			return candy[i].id;
+		}
+}	
+res.json(req.body.name);
+res.send(candy); 
+});
+
+
 module.exports = router;
+
+
+
+//update
+router.put('/', function(req, res) {
+var candyToAdd = req.params.name;
+
+candy.push (req.body.name);
+res.send(result);
+});
+*/
+
+//Implement index, create, update, show, and destroy functionality
+
